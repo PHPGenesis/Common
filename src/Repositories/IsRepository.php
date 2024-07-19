402bd97a3bc14c2897e8f3daaebf874c73291b5f
GenesisBot <ghbot@phpgenesis.com>
2024-07-19 18:28:56 +0000

    [Common] Add Method Skeletons to IsRepository Trait (#14)

--- src/Repositories/IsRepository.php
+++ src/Repositories/IsRepository.php
@@ -6,6 +6,8 @@
 
 namespace PHPGenesis\Common\Repositories;
 
+use EncoreDigitalGroup\StdLib\Exceptions\NotImplementedException;
+
 trait IsRepository
 {
     protected function arrayMapAttribute(string $property, string $attribute, array $attributes): void
@@ -13,5 +15,25 @@ trait IsRepository
         if (isset($attributes[$attribute])) {
             $this->$property = $attributes[$attribute];
         }
+    }
+
+    protected function map(): static
+    {
+        throw new NotImplementedException();
+    }
+
+    protected function mapModel(): static
+    {
+        throw new NotImplementedException();
+    }
+
+    protected function arrayMap(array $attributes): void
+    {
+        throw new NotImplementedException();
+    }
+
+    protected function fieldArray(): array
+    {
+        throw new NotImplementedException();
     }
 }
