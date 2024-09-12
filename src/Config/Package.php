89b9d7ab7ad18e58132f1a94265e8664e262f346
GenesisBot <ghbot@phpgenesis.com>
2024-09-12 16:10:39 +0000

    [OpenFeature] Add OpenFeature Implementation (#30)

---
+++ src/Config/Package.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\Common\Config;
+
+class Package
+{
+    public const string AWS = 'phpgenesis/aws';
+    public const string Common = 'phpgenesis/common';
+    public const string CLI = 'phpgenesis/cli';
+    public const string Logger = 'phpgenesis/logger';
+    public const string PhpGenesis = 'phpgenesis/phpgenesis';
+}
---
+++ src/Helpers/DirectoryHelper.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\Common\Helpers;
+
+class DirectoryHelper
+{
+    public static function basePath(): string
+    {
+        $directoryPath = __DIR__;
+        do {
+            $isVendorPath = false;
+            $currentDirectoryName = basename($directoryPath);
+            $directoryPath = dirname($directoryPath);
+            if ($currentDirectoryName == 'vendor') {
+                $isVendorPath = true;
+            }
+        } while ($isVendorPath == false);
+
+        return $directoryPath;
+    }
+}
