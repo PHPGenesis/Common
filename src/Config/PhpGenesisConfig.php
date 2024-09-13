a57074768de83055d8af8b0c3da9b4ad7a3c9998
GenesisBot <ghbot@phpgenesis.com>
2024-09-13 20:38:22 +0000

    [Logger] Merge Config Beta Feature (#32)

--- src/Config/PhpGenesisConfig.php
+++ src/Config/PhpGenesisConfig.php
@@ -6,9 +6,47 @@
 
 namespace PHPGenesis\Common\Config;
 
+use EncoreDigitalGroup\StdLib\Exceptions\NullExceptions\NullException;
+use PHPGenesis\Common\Helpers\DirectoryHelper;
+
 class PhpGenesisConfig
 {
-    const FILE_NAME = '/phpgenesis.json';
+    public static object $config;
+
+    const string FILE_NAME = '/phpgenesis.json';
 
     public ?object $logger;
+
+    public static function load(): ?object
+    {
+        $configFilePath = DirectoryHelper::basePath() . self::FILE_NAME;
+
+        if (file_exists($configFilePath)) {
+            $configFile = file_get_contents($configFilePath);
+
+            if ($configFile) {
+                $config = json_decode($configFile);
+                static::$config = $config;
+
+                return static::$config;
+            }
+        }
+
+        return null;
+    }
+
+    public static function get(): object
+    {
+        if (isset(static::$config)) {
+            return static::$config;
+        }
+
+        $config = static::load();
+
+        if (is_null($config)) {
+            throw new NullException('config');
+        }
+
+        return $config;
+    }
 }
