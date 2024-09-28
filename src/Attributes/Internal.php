0190d8be63e34ceb31cddf055e8007786a23f797
GenesisBot <ghbot@phpgenesis.com>
2024-09-28 21:27:25 +0000

    [Common] Create Internal Attribute (#39)

---
+++ src/Attributes/Internal.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\Common\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Internal {}
